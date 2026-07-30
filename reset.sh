26192036e8db4507733cebe3330d265d92a5f813
EojinHwang <jhin.h8@gmail.com>
2026-07-30 16:35:32 +0900

    [FIX]20260730_프로젝트 루트 경로 기준으로 Git 재설정 및 전체 환경 파일 업로드

--- reset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Docker container 와 volume을 중단 및 제거하는 중..."
-docker-compose down -v
-
-echo "Docker container들을 재시작 하는 중..."
-docker-compose up -d --build
-
-echo "Docker 환경이 재시작되어 준비되었습니다. 이 경로로 접속하세요. http://localhost:8080/"
